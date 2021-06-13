0e09374a20f4d834566ac6d83f2125578cd58b97
Fizer9 <demen9195@gmail.com>
2021-06-13 22:02:48 +0600

    0.19
    Added routing for messages and profile.
    Started working on the rest of the navigation items

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = (props) => {
     return (
-        <div className='app-wrapper'>
-            <Header />
-            <Navbar />
-            <div className='app-wrapper-content'>
-                <Dialogs />
-            </div>
-            {/*<Profile />*/}
-        </div>);
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header />
+                <Navbar />
+                <div className='app-wrapper-content'>
+                    <Route path='/profile' component={Profile} />
+                    <Route path='/dialogs' component={Dialogs} />
+                    <Route path='/news' component={News} />
+                    <Route path='/music' component={Music} />
+                    <Route path='/settings' component={Settings} />
+                </div>
+            </div>);
+        </BrowserRouter>
+    )
 }
 
 export default App;
