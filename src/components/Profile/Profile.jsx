fe7e64970f9d971c4a93fada7ae82469950a0487
Fizer9 <demen9195@gmail.com>
2021-06-13 14:00:09 +0600

    Social Network 0.17
    Transferring the project to github

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import s from './Profile.module.css';
+import MyPosts from './MyPosts/MyPosts';
+
+const Profile = () => {
+    return (
+        <div className={s.content}>
+            <div>
+                <img src="https://www.biletik.aero/upload/medialibrary/eb3/eb3395de4d9310e8b6a8e54c15eb3d7e.jpg" alt="#" />
+            </div>
+            <div>
+                ava + description
+            </div>
+            <MyPosts />
+        </div>
+    )
+}
+export default Profile;
