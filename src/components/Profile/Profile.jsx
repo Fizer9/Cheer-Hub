167737e833faf0e3eee9d1269149e6c01a59b95e
Fizer9 <demen9195@gmail.com>
2021-06-13 17:43:28 +0600

    0.18
    Added a dialog section

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src="https://www.biletik.aero/upload/medialibrary/eb3/eb3395de4d9310e8b6a8e54c15eb3d7e.jpg" alt="#" />
             </div>
