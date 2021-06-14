f54de1e37e5ce9e48a94cafb468c8da0b199af22
Fizer9 <demen9195@gmail.com>
2021-06-14 16:57:20 +0600

    0.22
    New design

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to='/dialogs/1'>Igor</NavLink>
+                </div>
+                <div className={s.dialog}>
+                <NavLink to='/dialogs/2'>Dasha</NavLink>
+                </div>
+                <div className={s.dialog}>
+                <NavLink to='/dialogs/3'>Sveta</NavLink>
+                </div>
+                <div className={s.dialog}>
+                <NavLink to='/dialogs/4'>Sasha</NavLink>
+                </div>
+                <div className={s.dialog}>
+                <NavLink to='/dialogs/5'>Egor</NavLink>
+                </div>
+                <div className={s.dialog}>
+                <NavLink to='/dialogs/6'>Andrey</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.messages}>Hi</div>
+                <div className={s.messages}>How are you?</div>
+                <div className={s.messages}>Hey</div>
+            </div>
         </div>
     )
 }
