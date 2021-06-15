0ae2278ed6238ab212366602137245a0aa1ea414
Fizer9 <demen9195@gmail.com>
2021-06-15 07:08:40 +0600

    0.23
    Added new component "ProfileInfo" from "Profile"

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
+import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = () => {
     return (
         <div>
-            <div>
-                <img src="https://www.biletik.aero/upload/medialibrary/eb3/eb3395de4d9310e8b6a8e54c15eb3d7e.jpg" alt="#" />
-            </div>
-            <div>
-                ava + description
-            </div>
+            <ProfileInfo />
             <MyPosts />
         </div>
     )
---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import s from './ProfileInfo.module.css';
+
+const ProfileInfo = () => {
+    return (
+        <div>
+            <div>
+                <img src="https://www.biletik.aero/upload/medialibrary/eb3/eb3395de4d9310e8b6a8e54c15eb3d7e.jpg" alt="#" />
+            </div>
+            <div>
+                ava + description
+            </div>
+        </div>
+    )
+}
+export default ProfileInfo;
