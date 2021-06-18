0c56488ce5231f5a4e38ea5bdfd486ab50f8b7b0
Fizer9 <demen9195@gmail.com>
2021-06-18 07:57:18 +0600

    0.24
    Created arrays: messageData, dialogsData and postData

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -19,6 +19,22 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+    let dialogsData = [
+        { id: 1, name: 'igor' },
+        { id: 2, name: 'Dasha' },
+        { id: 3, name: 'Sveta' },
+        { id: 4, name: 'Sasha' },
+        { id: 5, name: 'Egor' },
+        { id: 6, name: 'Andrey' }
+    ]
+    let messagesData = [
+        { id: 1, messages: 'Hi' },
+        { id: 2, messages: 'How is your day?' },
+        { id: 3, messages: 'Yo' },
+        { id: 4, messages: 'Yo' },
+        { id: 5, messages: 'Yo' }
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,12 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post'
 
 const MyPosts = () => {
+
+    let postData = [
+        { id: 1, messages: 'Hi, how are you?', likesCount: 12},
+        { id: 2, messages: "It's my first post", likesCount: 11},
+
+    ]
     return (
         <div>
             My posts
@@ -11,8 +17,8 @@ const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div className={s.posts}>
-                <Post message='Hi, how are you?' likesCount='0' />
-                <Post message="It's my first post" likesCount='23'/>
+                <Post message={postData[0].messages} likesCount={postData[0].likesCount} />
+                <Post message={postData[1].messages} likesCount={postData[1].likesCount} />
             </div>
         </div>
     )
