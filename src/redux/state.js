6eb1618547d6e5149773af17bab3e86c325378a4
Fizer9 <demen9195@gmail.com>
2021-07-27 16:05:41 +0600

    0.37

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,59 +1,60 @@
-let renderEntireTree = () => {
-    console.log('State changed');
-}
-
-let state = {
-    profilePage: {
-        postData: [
-            { id: 1, messages: 'Hi, how are you?', likesCount: 12 },
-            { id: 2, messages: "It's my first post", likesCount: 11 },
-        ],
-        newPostText: 'Value',
+let store = {
+    _state: {
+        profilePage: {
+            postData: [
+                { id: 1, messages: 'Hi, how are you?', likesCount: 12 },
+                { id: 2, messages: "It's my first post", likesCount: 11 },
+            ],
+            newPostText: 'Value',
+        },
+        dialogsPage: {
+            messages: [
+                { id: 1, messages: 'Hi' },
+                { id: 2, messages: 'Hello' },
+                { id: 3, messages: 'How is your day?' },
+                { id: 4, messages: 'Greate, how are you going' },
+                { id: 5, messages: 'Perfect, I just ended this section' },
+                { id: 6, messages: 'Yo, nice work' },
+            ],
+            dialogs: [
+                { id: 1, name: 'Igor', img: './Igor.jpg' },
+                { id: 2, name: 'Dasha', img: './Dasha.jpg' },
+                { id: 3, name: 'Konstantin', img: './Konstantin.jpg' },
+                { id: 4, name: 'Alexander', img: './Alexander.jpg' },
+                { id: 5, name: 'Anastasia', img: './Anastasia.jpg' },
+                { id: 6, name: 'Veronika', img: './Veronika.jpg' },
+            ],
+            newMessageText: 'Value',
+        },
     },
-    dialogsPage: {
-        messages: [
-            { id: 1, messages: 'Hi' },
-            { id: 2, messages: 'Hello' },
-            { id: 3, messages: 'How is your day?' },
-            { id: 4, messages: 'Greate, how are you going' },
-            { id: 5, messages: 'Perfect, I just ended this section' },
-            { id: 6, messages: 'Yo, nice work' },
-        ],
-        dialogs: [
-            { id: 1, name: 'Igor', img: './Igor.jpg' },
-            { id: 2, name: 'Dasha', img: './Dasha.jpg' },
-            { id: 3, name: 'Konstantin', img: './Konstantin.jpg' },
-            { id: 4, name: 'Alexander', img: './Alexander.jpg' },
-            { id: 5, name: 'Anastasia', img: './Anastasia.jpg' },
-            { id: 6, name: 'Veronika', img: './Veronika.jpg' },
-        ],
-        newMessageText: 'Value',
+    getState(){
+        return this._state;
     },
-}
-
-export let addPost = () => {
-    let newPost = {
-        id: 3,
-        messages: state.profilePage.newPostText,
-        likesCount: 0
-    };
-    state.profilePage.postData.push(newPost);
-    state.profilePage.newPostText = '';
-    renderEntireTree(state);
+    _callSubscriber() {
+        console.log('State changed');
+    },
+    addPost() {
+        debugger;
+        let newPost = {
+            id: 3,
+            messages: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+        this._state.profilePage.postData.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    updateNewMessageText(newText) {
+        this._state.dialogsPage.newMessageText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer; // observer
+    },
 };
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    renderEntireTree(state);
-};
-export const updateNewMessageText = (newText) => {
-    state.dialogsPage.newMessageText = newText;
-    renderEntireTree(state);
-};
 
-export const subscribe = (observer) => {
-    renderEntireTree = observer; // observer
-}
-
-export default state;
-
-//store
+export default store;
