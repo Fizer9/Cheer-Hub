89d1102220d93d2c965ea551886caff82fd2b537
Fizer9 <demen9195@gmail.com>
2021-09-18 13:27:33 +0600

    0.4.8
    Added Message container component (connect)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,9 +7,9 @@ import { sendMessageCreater, updateNewMessageBodyCreater } from './../../redux/d
 const Dialogs = (props) => {
     let state = props.dialogsPage;
 
-    let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} id={d.id} img={d.img} />);
+    let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} key={d.id} id={d.id} img={d.img} />);
 
-    let messagesElements = state.messages.map(m => <Message message={m.message} id={m.id} />);
+    let messagesElements = state.messages.map(m => <Message message={m.message} key={m.id} />);
 
     let newMessageBody = state.newMessageBody;
 
--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,3 +1,4 @@
+
 const SEND_MESSAGE = 'SEND_MESSAGE';
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
 
@@ -7,8 +8,8 @@ let initialState = {
         { id: 2, message: 'Hello' },
         { id: 3, message: 'How is your day?' },
         { id: 4, message: 'Greate, how are you going' },
-        { id: 5, message: 'Perfect, I just ended this section' },
-        { id: 6, message: 'Yo, nice work' },
+        { id: 5, message: 'Perfect, I just ended this Page' },
+        { id: 6, message: 'Nice work!' },
     ],
     dialogs: [
         { id: 1, name: 'Igor', img: './Igor.jpg' },
@@ -20,24 +21,27 @@ let initialState = {
     ],
     newMessageBody: '',
 };
-
-const dialogsReducer = (state = initialState, action) => {
+const dialogsReducer = (state = initialState, action) => {   
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
-            state.newMessageBody = action.body;
-            return state;
-        case SEND_MESSAGE:
-            let body = state.newMessageBody;
-            state.newMessageBody = '';
-            state.messages.push({ id: 7, message: body });
-            return state;
+            return {
+                ...state,
+                newMessageBody: action.body
+            };
+        case SEND_MESSAGE: 
+        let body = state.newMessageBody;
+        return {
+            ...state,
+            newMessageBody: '',
+            messages: [...state.messages, { id: 7, message: body }]
+        };
         default:
             return state;
     }
 }
 
-export const sendMessageCreater = () => ({ type: 'SEND_MESSAGE' })
+export const sendMessageCreater = () => ({ type: SEND_MESSAGE })
 export const updateNewMessageBodyCreater = (body) =>
-    ({ type: 'UPDATE_NEW_MESSAGE_BODY', body: body })
+    ({ type: UPDATE_NEW_MESSAGE_BODY, body: body })
 
 export default dialogsReducer;
