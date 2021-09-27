9c12c27c89635a5429e4d5fad552280240ab2651
Fizer9 <demen9195@gmail.com>
2021-09-27 23:59:39 +0600

    0.5.1
    Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,22 +1,25 @@
+import * as axios from 'axios';
 import styles from './Users.module.css';
+import userPhoto from '../../assets/images/user.png';
 
 let Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-            { id: 1, photoUrl: 'https://i1.sndcdn.com/artworks-vv68F64hlnO8Seqr-hgYb8w-t500x500.jpg', followed: false, fullName: 'Igor', status: 'I am looking for a Job', location: { city: 'Omsk', country: 'Russia' } },
-            { id: 2, photoUrl: 'https://i1.sndcdn.com/artworks-vv68F64hlnO8Seqr-hgYb8w-t500x500.jpg', followed: true, fullName: 'Dmitry', status: 'Sleep', location: { city: 'Moscow', country: 'Russia' } },
-            { id: 3, photoUrl: 'https://i1.sndcdn.com/artworks-vv68F64hlnO8Seqr-hgYb8w-t500x500.jpg', followed: true, fullName: 'Kostya', status: 'Studing', location: { city: 'Kiev', country: 'Ukrain' } }
-        ])
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items)
+        });
+
+
     }
     return <div>
         {
             props.users.map(u => <div key={u.id}>
-                <div><img src={u.photoUrl} alt="" /></div>
-                <div>{u.fullName}</div>
+                <div><img src={ u.photos.small != null ? u.photos.small : userPhoto } alt="" /></div>
+                <div>{u.name}</div>
                 <div>{u.status}</div>
-                <div>{u.location.country}</div>
-                <div>{u.location.city}</div>
+                <div>{"u.location.country"}</div>
+                <div>{"u.location.city"}</div>
             </div>)
         }
     </div>
