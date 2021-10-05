8606dcc27d5be6228b21400312a8558d1e0ef75b
Fizer9 <demen9195@gmail.com>
2021-10-05 21:37:00 +0600

    0.5.4
    Added get request in User component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,25 +5,24 @@ import userPhoto from '../../assets/images/user.png';
 
 class Users extends React.Component {
 
-    constructor(props){
-        super(props)
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                this.props.setUsers(response.data.items)
-            });
+            this.props.setUsers(response.data.items)
+        })
     }
 
-    render(){
+    render() {
         return <div>
-        {
-            this.props.users.map(u => <div key={u.id}>
-                <div><img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" /></div>
-                <div>{u.name}</div>
-                <div>{u.status}</div>
-                <div>{"u.location.country"}</div>
-                <div>{"u.location.city"}</div>
-            </div>)
-        }
-    </div>
+            {
+                this.props.users.map(u => <div key={u.id}>
+                    <div><img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" /></div>
+                    <div>{u.name}</div>
+                    <div>{u.status}</div>
+                    <div>{"u.location.country"}</div>
+                    <div>{"u.location.city"}</div>
+                </div>)
+            }
+        </div>
     }
 }
 
