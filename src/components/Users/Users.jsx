13fb63d4d62c9a1c14d53bb92c8d1faf5fd969a6
Fizer9 <demen9195@gmail.com>
2021-11-01 09:18:23 +0600

    0.5.7
    Preloader component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,50 +1,32 @@
-import React from 'react';
-import * as axios from 'axios';
+import React from "react";
 import styles from './Users.module.css';
 import userPhoto from '../../assets/images/user.png';
 
-class Users extends React.Component {
+let Users = (props) => {
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
-            this.props.setTotalUsersCount(response.data.totalCount)
-        })
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
+
+    let pages = [];
+    for (let i=1; i <= pagesCount; i++){
+        pages.push(i);
     }
 
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
-        })
-    }
-
-    render() {
-
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
-
-        let pages = [];
-        for (let i=1; i <= pagesCount; i++){
-            pages.push(i);
-        }
-
-        return <div>
-            <div>
-        {pages.map( p => {
-            return <span onClick={(e) => { this.onPageChanged(p) }} className={this.props.currentPage === p && styles.selectedPage}>{p}</span>
-        })}
-            </div>
-            {
-                this.props.users.map(u => <div key={u.id}>
-                    <div><img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" /></div>
-                    <div>{u.name}</div>
-                    <div>{u.status}</div>
-                    <div>{"u.location.country"}</div>
-                    <div>{"u.location.city"}</div>
-                </div>)
-            }
+    return <div>
+        <div>
+            {pages.map(p => {
+                return <span className={props.currentPage === p && styles.selectedPage} onClick={(e) => { props.onPageChanged(p) }}>{p}</span>
+            })}
         </div>
-    }
+        {
+            props.users.map(u => <div key={u.id}>
+                <div><img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" /></div>
+                <div>{u.name}</div>
+                <div>{u.status}</div>
+                <div>{"u.location.country"}</div>
+                <div>{"u.location.city"}</div>
+            </div>)
+        }
+    </div>
 }
 
-export default Users;
+export default Users
---
+++ src/components/common/Preloader/Preloader.js
@@ -0,0 +1,9 @@
+import loader from '../../../assets/images/loader.svg'
+
+let Preloader = (props) => {
+    return <div>
+        <img src={loader} />
+    </div>
+}
+
+export default Preloader
