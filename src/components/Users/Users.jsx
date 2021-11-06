917e698d71bcf57eb30a78849d18e5556b9d8784
Fizer9 <demen9195@gmail.com>
2021-11-06 20:03:18 +0600

    0.5.8
    Added Navlinks for user images and subscribe button

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from './Users.module.css';
 import userPhoto from '../../assets/images/user.png';
+import { NavLink } from "react-router-dom";
 
 let Users = (props) => {
 
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
 
     let pages = [];
-    for (let i=1; i <= pagesCount; i++){
+    for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
 
@@ -19,7 +20,20 @@ let Users = (props) => {
         </div>
         {
             props.users.map(u => <div key={u.id}>
-                <div><img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" /></div>
+                <div>
+                    <NavLink to={'/profile/' + u.id}>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" />
+                    </NavLink>
+                </div>
+                <div>
+                    {u.followed
+                        ? <button onClick={() => {
+                            props.unfollow(u.id)
+                        }}>Unfollow</button>
+                        : <button onClick={() => {
+                            props.follow(u.id)
+                        }}>Follow</button>}
+                </div>
                 <div>{u.name}</div>
                 <div>{u.status}</div>
                 <div>{"u.location.country"}</div>
