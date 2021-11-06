ab332c4173e84099f57dff6f045307838ca488e1
Fizer9 <demen9195@gmail.com>
2021-11-07 03:22:56 +0600

    0.5.9
    Profile page, ajax, api

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import Preloader from '../../common/Preloader/Preloader';
 import s from './ProfileInfo.module.css';
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+    if (!props.profile) {
+        return <Preloader />
+    }
+
     return (
         <div>
             <div>
                 <img src="https://www.biletik.aero/upload/medialibrary/eb3/eb3395de4d9310e8b6a8e54c15eb3d7e.jpg" alt="#" />
             </div>
             <div>
+                <img src={props.profile.photos.large} />
                 ava + description
             </div>
         </div>
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,5 +1,6 @@
 const ADD_POST = 'ADD_POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE_NEW_POST_TEXT';
+const SET_USER_PROFILE = 'SET_USER_PROFILE';
 
 let initialState = {
     posts: [
@@ -7,6 +8,7 @@ let initialState = {
         { id: 2, messages: "It's my first post", likesCount: 11 },
     ],
     newPostText: '',
+    profile: null
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -29,12 +31,16 @@ const profileReducer = (state = initialState, action) => {
                 newPostText: action.newText
             };
         }
+        case SET_USER_PROFILE: {
+            return {...state, profile: action.profile}
+        }
         default:
             return state;
     }
 }
 
-export const addPostActionCreater = () => ({ type: 'ADD_POST' })
+export const addPostActionCreater = () => ({ type: ADD_POST })
+export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile })
 export const updateNewPostTextActionCreater = (text) => ({
     type: 'UPDATE_NEW_POST_TEXT', newText: text
 })
