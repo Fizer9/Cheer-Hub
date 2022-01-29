2eb7b739640be4821cd665372309d1a4fb7ede6a
Fizer9 <demen9195@gmail.com>
2022-01-29 17:48:29 +0600

    0.7.55

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import s from './Header.module.css';
 
-const Header = (props) => {        
+const Header = (props) => {
         return <header className={s.header}>
 
         <div className={s.loginBlock}>
